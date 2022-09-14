543fcd609f639214c05f1bc84965f809d2f1f097
Christopher Capobianco <ccapo@users.noreply.github.com>
2022-09-14 01:02:09 -0400

    Update default port

--- env_generation.py
+++ env_generation.py
@@ -10,6 +10,6 @@ jwt_secret = create_token()
 
 print(f"API_TOKEN={api_token.decode()}")
 print(f"JWT_SECRET={jwt_secret.decode()}")
-print(f"PORT=5050")
+print(f"PORT=80")
 print(f"ENV=dev")
 print(f"DEBUG=True")
